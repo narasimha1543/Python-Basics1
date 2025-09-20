ec87fa48bf078feb580c7bf5c46bb05a0a44be86
narasimha1543 <mlnreddy2007@gmail.com>
2025-09-20 11:32:07 +0530

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,37 @@
+# file=open("students.txt","r")
+# content=file.read()
+# print(content)
+# file.close() 
+
+#types of read():
+#1. read() 
+# file=open("students.txt","r")
+# content=file.read()
+# print(content)
+# file.close() 
+
+#2. readline() 
+# file=open("students.txt","r")
+# content=file.readline()
+# content1=file.readline()
+# content2=file.readline()
+# print(content)
+# print(content1)
+# print(content2)
+# file.close() 
+
+#3. readlines()
+# file=open("students.txt","r") 
+# content=file.readlines()
+# print(content)
+# file.close()
+
+file=open("student.txt","a")
+#write,writelines
+file.write("hello c++\n")
+file.close()
+
+
+
+
+
